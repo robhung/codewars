81c6517a199eee98d0a9b2e5f4b4ada0313a6c7d
Robert Hung <rph.hung@gmail.com>
2020-02-09 18:14:14 +0800

    Answer

--- 6kyu/WeIrD-StRiNg-CaSe/toWeirdCase.js
+++ 6kyu/WeIrD-StRiNg-CaSe/toWeirdCase.js
@@ -1,5 +1,17 @@
 function toWeirdCase(string) {
-  return 'ThIs Is A TeSt';
+  const isEven = num => num % 2 === 0;
+
+  const wordArr = string.split(' ');
+  const newWordArr = wordArr.map(word => {
+    const characterArr = word.split('');
+    const newCharacterArr = characterArr.map((letter, index) =>
+      isEven(index) ? letter.toUpperCase() : letter.toLowerCase()
+    );
+
+    return newCharacterArr.join('');
+  });
+
+  return newWordArr.join(' ');
 }
 
 module.exports = toWeirdCase;
--- 6kyu/WeIrD-StRiNg-CaSe/toWeirdCase.test.js
+++ 6kyu/WeIrD-StRiNg-CaSe/toWeirdCase.test.js
@@ -6,6 +6,7 @@ describe('toWeirdCase', function() {
     expect(toWeirdCase('is')).toBe('Is');
   });
   it('should return the correct value for multiple words', function() {
+    expect(toWeirdCase('Weird string case')).toBe('WeIrD StRiNg CaSe');
     expect(toWeirdCase('This is a test')).toBe('ThIs Is A TeSt');
   });
 });
